e20aff9651a1aeae98497c79e48cbbae0281b857
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2023-11-19 20:40:20 +0700

    update data_project.js

--- data_project.js
+++ data_project.js
@@ -6,10 +6,14 @@ if (projectId === 'startjourneyella') {
     document.getElementById('textProject').innerHTML = 'Ellatheria Mempersembahkan Project untuk Sonichi Show Theater di Setlist Seishun Girls. #StartJourneyElla.';
     document.getElementById('tanggalProject').innerHTML = '19 Maret 2022';
     document.getElementById('imageUtama').src = 'https://pbs.twimg.com/media/FONmfgaVIAoXXI5?format=jpg&name=small';
-    document.getElementById('gallery1').src = 'https://pbs.twimg.com/media/FOWeyBuUYAMGuNf?format=jpg&name=large';
-    document.getElementById('gallery2').src = 'https://pbs.twimg.com/media/FOTHIhOVkAAiI_t?format=jpg&name=small';
-    document.getElementById('gallery3').src = 'https://pbs.twimg.com/media/FOTHIgxVgAELda9?format=jpg&name=small';
+    document.getElementById('gallery1').outerHTML = '<a href="https://pbs.twimg.com/media/FOWeyBuUYAMGuNf?format=jpg&name=large" target="_blank">' +
+            '<img id="gallery1" src="https://pbs.twimg.com/media/FOWeyBuUYAMGuNf?format=jpg&name=large" alt=""></a>';
+    document.getElementById('gallery2').outerHTML = '<a href="https://pbs.twimg.com/media/FOTHIhOVkAAiI_t?format=jpg&name=small" target="_blank">' +
+            '<img id="gallery2" src="https://pbs.twimg.com/media/FOTHIhOVkAAiI_t?format=jpg&name=small" alt=""></a>';
+    document.getElementById('gallery3').outerHTML = '<a href="https://pbs.twimg.com/media/FOTHIgxVgAELda9?format=jpg&name=small" target="_blank">' +
+            '<img id="gallery3" src="https://pbs.twimg.com/media/FOTHIgxVgAELda9?format=jpg&name=small" alt=""></a>';
     
+
     // Menyesuaikan href tombol
     
     
@@ -20,15 +24,15 @@ else if (projectId === 'kaizokuella16') {
     document.getElementById('textProject').innerHTML = 'Bagaikan Bajak Laut yang melakukan perjalanan menuju harta karun, begitu pun Ella yang melakukan perjalanannya selama hampir 16 tahun (saat ini) untuk menuju ke cita-citanya. 🤍<br><br> Dengan ini kami persembahkan project ulang tahun ke 16 Ella yang bertajuk #KaizokuElla16';
     document.getElementById('tanggalProject').innerHTML = '3 Agustus 2022';
     document.getElementById('imageUtama').src = 'https://pbs.twimg.com/media/FZfY3SgUYAA9D-J?format=jpg&name=small';
-    document.getElementById('gallery1').outerHTML = '<a href="https://twitter.com/AM_EllaJKT48/status/1561315288661929985?t=xM6qcUvHUJJgLYjIxZO_kg&s=19" target="_blank">' +
+    document.getElementById('gallery1').outerHTML = '<a href="https://pbs.twimg.com/media/Farn1hWUsAE_1Wp?format=jpg&name=medium" target="_blank">' +
     '<img id="gallery1" src="https://pbs.twimg.com/media/Farn1hWUsAE_1Wp?format=jpg&name=medium" alt=""></a>';
-    document.getElementById('gallery2').outerHTML = '<a href="https://twitter.com/EllatheriaOfc/status/1554754081100800000?t=S-COGZN-NAhP4K3LqtvFBA&s=19" target="_blank">' +
+    document.getElementById('gallery2').outerHTML = '<a href="https://pbs.twimg.com/media/Faq7Br8agAA2I9o?format=jpg&name=medium" target="_blank">' +
         '<img id="gallery2" src="https://pbs.twimg.com/media/Faq7Br8agAA2I9o?format=jpg&name=medium" alt=""></a>';
         
-document.getElementById('gallery3').outerHTML = '<a href="https://twitter.com/EllatheriaOfc/status/1554754081100800000?t=S-COGZN-NAhP4K3LqtvFBA&s=19" target="_blank">' +
+document.getElementById('gallery3').outerHTML = '<a href="https://pbs.twimg.com/media/Far1nWfUcAAWIFd?format=jpg&name=900x900" target="_blank">' +
         '<img id="gallery3" src="https://pbs.twimg.com/media/Far1nWfUcAAWIFd?format=jpg&name=900x900" alt=""></a>';
         
-   document.getElementById('gallery4').outerHTML = '<a href="https://twitter.com/EllatheriaOfc/status/1554754081100800000?t=S-COGZN-NAhP4K3LqtvFBA&s=19" target="_blank">' +
+   document.getElementById('gallery4').outerHTML = '<a href="https://pbs.twimg.com/media/Far1mkCVsAEQ5dy?format=jpg&name=900x900" target="_blank">' +
         '<img id="gallery4" src="https://pbs.twimg.com/media/Far1mkCVsAEQ5dy?format=jpg&name=900x900" alt=""></a>';
     document.getElementById('gallery5').src = '';
     document.getElementById('gallery6').src = '';
@@ -43,7 +47,7 @@ else if (projectId === 'anniversary2abigailuxe') {
     document.getElementById('textProject').innerHTML = 'Tak terasa 2 tahun Fanbase ini berdiri untuk mendukung <a style="color: #FFAC39; "href="https://twitter.com/AM_EllaJKT48">@AM_EllaJKT48</a> .<br>Banyak yg di lalu baik senang walaupun sedih.<br><br> Terimakasih atas dukungannya untuk Fanbase ini selama 2 tahun ini, harapannya agar ke depannya semua selalu solid untuk mendukung Ella !<br>#Abigailuxe2earchDream';
     document.getElementById('tanggalProject').innerHTML = '28 Agustus 2022';
     document.getElementById('imageUtama').src = 'https://pbs.twimg.com/media/FbNa2T_UIAAtlYA?format=jpg&name=small';
-    document.getElementById('gallery1').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1563743174354702336?s=20" target="_blank">' +
+    document.getElementById('gallery1').outerHTML = '<a href="https://pbs.twimg.com/media/FbOIH7XVUAErCjh?format=jpg&name=medium" target="_blank">' +
         '<img id="gallery" src="https://pbs.twimg.com/media/FbOIH7XVUAErCjh?format=jpg&name=medium" alt=""></a>';
 document.getElementById('').outerHTML = '<a href="URL_tautan_gambar1" target="_blank">' +
             '<img id="gallery" src="" alt=""></a>';
@@ -66,17 +70,17 @@ else if (projectId === 'arc17ellaterland') {
     document.getElementById('textProject').innerHTML = 'Project perayaan ulang tahun Ella ke 17. <br><br>Imagine. Once you reach seventeen, a place that is never to be seen, appeared upon you. What would you do? <br><br>An island full of happiness and people who loves you that will stand by your side on your way to adulthood.<br><br>#Arc17Ellaterland<br><br>We proudly presents“ Ellaterland”, a project specially made for<a style="color: #FFAC39; "href="https://twitter.com/AM_EllaJKT48">@AM_EllaJKT48</a>’ s Sweet seventeen birthday.';
     document.getElementById('tanggalProject').innerHTML = '6 Agustus 2023';
     document.getElementById('imageUtama').src = 'https://pbs.twimg.com/media/F22-H5RaYAAKC2e?format=jpg&name=small';
-    document.getElementById('gallery1').outerHTML = '<a href="https://twitter.com/AM_EllaJKT48/status/1690036579455414272?t=Mz4lM1QmUX2zNJvsr1Sxmw&s=19" target="_blank">' +
+    document.getElementById('gallery1').outerHTML = '<a href="https://pbs.twimg.com/media/F3Q3RhBbUAASZ7j?format=jpg&name=900x900" target="_blank">' +
         '<img id="gallery1" src="https://pbs.twimg.com/media/F3Q3RhBbUAASZ7j?format=jpg&name=900x900" alt=""></a>';
-document.getElementById('gallery2').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1690387628837961728?s=20" target="_blank">' +
+document.getElementById('gallery2').outerHTML = '<a href="https://pbs.twimg.com/media/F3Zhj52asAESxTL?format=jpg&name=900x900" target="_blank">' +
             '<img id="gallery2" src="https://pbs.twimg.com/media/F3Zhj52asAESxTL?format=jpg&name=900x900" alt=""></a>';
-document.getElementById('gallery3').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1690387628837961728?s=20" target="_blank">' +
+document.getElementById('gallery3').outerHTML = '<a href="https://pbs.twimg.com/media/F3Zhj6AbMAA2Xn7?format=jpg&name=900x900" target="_blank">' +
         '<img id="gallery3" src="https://pbs.twimg.com/media/F3Zhj6AbMAA2Xn7?format=jpg&name=900x900" alt=""></a>';
-document.getElementById('gallery4').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1690387628837961728?s=20" target="_blank">' +
+document.getElementById('gallery4').outerHTML = '<a href="https://pbs.twimg.com/media/F3V2jZiaEAA02CG?format=jpg&name=small" target="_blank">' +
                     '<img id="gallery4" src="https://pbs.twimg.com/media/F3V2jZiaEAA02CG?format=jpg&name=small" alt=""></a>';
-document.getElementById('gallery5').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1690387628837961728?s=20" target="_blank">' +
+document.getElementById('gallery5').outerHTML = '<a href="https://pbs.twimg.com/media/F3V2jZkbIAAv21i?format=jpg&name=small" target="_blank">' +
                     '<img id="gallery5" src="https://pbs.twimg.com/media/F3V2jZkbIAAv21i?format=jpg&name=small" alt=""></a>';
-document.getElementById('gallery6').outerHTML = '<a href="https://x.com/EllatheriaOfc/status/1690387628837961728?s=20" target="_blank">' +
+document.getElementById('gallery6').outerHTML = '<a href="https://pbs.twimg.com/media/F3V2jZrasAAgHkm?format=jpg&name=small" target="_blank">' +
                     '<img id="gallery6" src="https://pbs.twimg.com/media/F3V2jZrasAAgHkm?format=jpg&name=small" alt=""></a>';
     // Menyesuaikan href tombol
     
@@ -108,7 +112,7 @@ else if (projectId === 'merchandisevol1') {
     document.getElementById('textProject').innerHTML = '[ELLATHERIA PROUDLY PRESENT]<br>Official Merchandise Vol.1 telah dibuka 🔥<br><br>Setiap pembelian bundle package ini juga akan mendapatkan bonus Photo Card dan Stiker!<br><br>So, buruan checkout barang yang kalian inginkan!<br><br>Order via link berikut ini; <br><a style="color: #FFAC39; "href="https://ellatheria.my.id/merchandise">https://ellatheria.my.id/merchandise</a><br>atau<br><a style="color: #FFAC39; "href="https://tokopedia.com/ellatheriaofc">https://tokopedia.com/ellatheriaofc</a>';
     document.getElementById('tanggalProject').innerHTML = '18 Oktober 2023';
     document.getElementById('imageUtama').src = 'https://pbs.twimg.com/media/F8ufGaIWQAArnSV?format=jpg&name=medium';
-    document.getElementById('gallery1').outerHTML = '<a href="" target="_blank">' +
+    document.getElementById('gallery1').outerHTML = '<a href="https://ellatheria.my.id/merchandiseimg/merchandise_banner.jpg" target="_blank">' +
         '<img id="gallery" src="merchandiseimg/merchandise_banner.jpg" alt=""></a>';
 document.getElementById('').outerHTML = '<a href="URL_tautan_gambar1" target="_blank">' +
             '<img id="gallery" src="" alt=""></a>';
